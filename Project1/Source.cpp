4868575688702f118c63545158fd28a341367bc1
Fizinftop99 <naumov_nik99@mail.ru>
2020-03-16 22:25:12 +0300

    first 5 tasks

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -17,20 +17,27 @@ public:
 
 int main()
 {
+    //std::cout <<( 5 % 2) == 1;
     std::vector<int> v;
+    // создание последовательности
     std::generate_n(back_inserter(v), 10, IntSequence(1));
+    // добавка в конец из cin
     std::istream_iterator<int> intReader(std::cin);
     std::istream_iterator<int> intReaderEOF;
     std::copy(intReader, intReaderEOF, back_inserter(v));
+    // перемешивание
     std::random_shuffle(v.begin(), v.end());
+    // удаление дубликатов
     std::sort(v.begin(), v.end());
     auto pos = std::unique(v.begin(), v.end());
     std::ostream_iterator<int> intWriter(std::cout, " ");
 	std::copy(v.begin(), pos, intWriter);
-    std::cout << std::count(v.begin(), v.end(),
+    std::cout << '\n';
+    // подсчет нечетных
+    std::cout << count_if(v.begin(), v.end(),
                         [](int elem) {
                             return (elem%2==1);
                         });
-	std::copy(v.begin(), v.end(), intWriter);
+	
 	return 0;
 }
